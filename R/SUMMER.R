4c2b3018d21ed697675a830846f28b0238bee2af
martinbryan <bryandmartin1@gmail.com>
2017-11-15 21:13:20 -0800

    Minor typo fix

--- R/SUMMER.R
+++ R/SUMMER.R
@@ -1,6 +1,6 @@
 #' SUMMER package documentation.
 #'
-#' SUMMER provides implementation for Spatiotemporal Under-five Mortality Methods for Estimation in R.
+#' SUMMER provides implementation for Spatio-temporal Under-five Mortality Methods for Estimation in R.
 #'
 #' The package will be maintained on \url{https://github.com/martinbryan/SUMMER}.
 #'
