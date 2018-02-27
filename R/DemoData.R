2c122f2056785777e31d742ac4dbed6553a79a5a
martinbryan <bryandmartin1@gmail.com>
2018-02-27 13:10:27 -0800

    Remove Uganda references everywhere but vignette. Vignette now in test mode

--- R/DemoData.R
+++ R/DemoData.R
@@ -1,9 +1,9 @@
-#' Fake Uganda dataset for vignette. 
+#' Fake dataset for vignette. 
 #' 
-#' A small fake dataset with 4 regions and 5 survey years. These numbers do not represent 
+#' A small fake dataset with 4 regions and 5 survey years. This  does not represent 
 #' any real country's data and are based on a subset of the model dataset
 #' provided by DHS.
 #'
 #' @format A list of with five components, named by survey year.
 #' @source \url{https://dhsprogram.com/data/model-datasets.cfm}
-"Uganda"
+"DemoData"
--- R/DemoMap.R
+++ R/DemoMap.R
@@ -1,10 +1,10 @@
-#' Uganda map dataset for vignette. 
+#' Map dataset for vignette. 
 #' 
-#' Shapefiles are from 1995 Uganda Admin 1 regions provided by DHS.
+#' Shapefiles are from 1995 Uganda Admin 1 regions provided by DHS, but the data .
 #' 
 #' \itemize{
 #'  \item geo. Geographic map files
 #'  \item Amat. Adjacency matrix for regions
 #' }
 #' @source \url{https://spatialdata.dhsprogram.com/boundaries/#view=table&countryId=UG}
-"UgandaMap"
+"DemoMap"
