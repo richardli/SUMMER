decbb915a2d461b03562f12f523da678f9aced93
Richard Li <lizehang@gmail.com>
2019-03-26 15:15:21 -0400

    fix default hyper priors

--- R/fitINLA.R
+++ R/fitINLA.R
@@ -184,7 +184,7 @@ fitINLA <- function(data, Amat, geo, formula = NULL, rw = 2, is.yearly = TRUE, y
       if (!exists("my.cache", envir = envir, mode = "list")) {
         nn = n %/% m
         stopifnot (nn == as.integer(n/m))
-        R = Diagonal(n, x = rep(1, n))
+        R = Matrix::Diagonal(n, x = rep(1, n))
         A = matrix(0, nn, n)
         j = 1
         for(i in 1:nn) {
@@ -205,7 +205,7 @@ fitINLA <- function(data, Amat, geo, formula = NULL, rw = 2, is.yearly = TRUE, y
       }
       
       Q = function() {
-        QQ = rbind(cbind(p$kappa * my.cache$R + tau * t(my.cache$A) %*% my.cache$A,
+        QQ = rbind(cbind(p$kappa * my.cache$R + tau *  t(my.cache$A) %*% my.cache$A,
                          -tau * t(my.cache$A)),
                    cbind(-tau * my.cache$A, tau * my.cache$D))
         return(QQ)
@@ -262,9 +262,9 @@ fitINLA <- function(data, Amat, geo, formula = NULL, rw = 2, is.yearly = TRUE, y
     if (!exists("my.cache", envir = envir, mode = "list")) {
       nn = n %/% m
       stopifnot (nn == as.integer(n/m))
-      R1 = Diagonal(n, x = rep(1, n))
+      R1 = Matrix::Diagonal(n, x = rep(1, n))
       R2 = inla.rw(n, order = order, scale.model=TRUE, sparse=TRUE)
-      R3 = Diagonal(S, x = rep(1, S))
+      R3 = Matrix::Diagonal(S, x = rep(1, S))
       R4 = Amat
       diag(R4) <- 0
       diag <- apply(R4, 1, sum)
@@ -293,7 +293,7 @@ fitINLA <- function(data, Amat, geo, formula = NULL, rw = 2, is.yearly = TRUE, y
         j = j + m
       }
       A = inla.as.sparse(A)
-      D = Diagonal(nn*S, x=1)
+      D = Matrix::Diagonal(nn*S, x=1)
       assign("my.cache", list(R=INLA::inla.as.sparse(R), A=A, D=D, nn=nn), envir = envir)
     } 
     
@@ -371,18 +371,18 @@ fitINLA <- function(data, Amat, geo, formula = NULL, rw = 2, is.yearly = TRUE, y
     #################################################################### Re-calculate hyper-priors
     # Todo: make it work with the new Q matrix!!
     
-    if (redo.prior) {
+    if (redo.prior || is.null(priors)) {
       priors <- simhyper(R = 2, nsamp = 1e+05, nsamp.check = 5000, Amat = Amat, nperiod = length(year_names))
     }
     
-    a.iid <- priors$a.iid
-    b.iid <- priors$b.iid
-    a.rw1 <- priors$a.iid
-    b.rw1 <- priors$a.iid
-    a.rw2 <- priors$a.iid
-    b.rw2 <- priors$a.iid
-    a.icar <- priors$a.iid
-    b.icar <- priors$a.iid
+    if(is.null(a.iid)) a.iid <- priors$a.iid
+    if(is.null(b.iid)) b.iid <- priors$b.iid
+    if(is.null(a.rw1)) a.rw1 <- priors$a.iid
+    if(is.null(b.rw1)) b.rw1 <- priors$b.iid
+    if(is.null(a.rw2)) a.rw2 <- priors$a.iid
+    if(is.null(b.rw2)) b.rw2 <- priors$b.iid
+    if(is.null(a.icar)) a.icar <- priors$a.iid
+    if(is.null(b.icar)) b.icar <- priors$b.iid
     
     #################################################################### # remove NA rows? e.g. if no 10-14 available
     if (na.rm) {
