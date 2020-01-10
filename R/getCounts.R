1077b49a545e60ee9b78746fba2812a086616da2
Richard Li <lizehang@gmail.com>
2020-01-10 11:32:05 -0800

    allow getCounts to drop rows with unseen combinations

--- R/getCounts.R
+++ R/getCounts.R
@@ -5,6 +5,7 @@
 #' @param by a character vector of columns that specifies which groups to aggregate by.
 #' @param ignore list of conditions not to impute 0. If left unspecified, any group levels not in the data will be imputed to have 0 counts. 
 #' @param addtotal logical indicator of whether to add a column of group total counts.
+#' @param drop logical indicator of whether to drop all rows with total = 0.
 #' 
 #' @return data.frame of the ggregated counts. 
 #' @examples
@@ -27,14 +28,14 @@
 #' @export
 
 
-getCounts <- function(data, variables, by, ignore = NULL, addtotal = TRUE){
+getCounts <- function(data, variables, by, ignore = NULL, addtotal = TRUE, drop=FALSE){
 	if(addtotal){
 		data$total <- 1
 		variables <- c(variables, "total")
 	}
 	data <- data[, c(variables, by)]
 	formula <- as.formula(paste0(".~", paste(by, collapse = " + ")))
-	out <- aggregate(formula, data = data, FUN = sum, drop = FALSE)
+	out <- aggregate(formula, data = data, FUN = sum, drop = drop)
 	for(v in variables){
 		out[is.na(out[, v]), v] <- 0
 	}
