66023be12fe2af219e747557ae5dade95e6e5422
Richard Li <lizehang@gmail.com>
2020-03-08 15:40:02 -0400

    add Malawi Map to the package

--- R/DemoMap2.R
+++ R/DemoMap2.R
@@ -1,10 +1,10 @@
-#' Map dataset for vignette. 
+#' Kenya map dataset for vignette. 
 #' 
-#' Shapefiles are from 2014 Kenya Admin 1 regions provided by DHS, but the data do not represent real information about any country.
+#' Shapefiles are from 2014 Kenya Admin 1 regions provided by DHS.
 #' 
 #' \itemize{
-#'  \item geo. Geographic map files
-#'  \item Amat. Adjacency matrix for regions
+#'  \item geo Geographic map files
+#'  \item Amat Adjacency matrix for regions
 #' }
 #' @source \url{https://spatialdata.dhsprogram.com/boundaries/#view=table&countryId=KE}
 #' @docType data
---
+++ R/MalawiMap.R
@@ -0,0 +1,8 @@
+#' Malawi map dataset for vignette. 
+#' 
+#' SpatialPolygonsDataFrame objects that reflect the Admin 2 regions in Malawi, including the Likoma island. The Admin 2 region names are in the ADM2_EN field.
+#' 
+#' @docType data
+#' @usage data(malawiMap)
+
+"malawiMap"
