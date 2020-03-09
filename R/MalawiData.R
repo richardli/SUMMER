81d4117a3d13023f6abda0345b72e78f4d954635
Richard Li <lizehang@gmail.com>
2020-03-09 10:49:21 -0400

    add Malawi HIV and IGME data to package

---
+++ R/MalawiData.R
@@ -0,0 +1,17 @@
+#' Auxiliary data for Malawi 2000, 2004, 2010, and 2015 DHS. 
+#' 
+#' The list contains several data frames.  
+#' 
+#' 
+#' \itemize{
+#'  \item HIV, a data frame with three columns: years (in five year periods),  survey, and the estimated bias of the reported U5MR due to HIV for each 5 year period. The bias is represented as the ratio of the reported U5MR to the true U5MR.
+#'  \item HIV.yearly, a data frame with three columns: years (in one year interval),  survey, and the estimated bias of the reported U5MR due to HIV for each year. The bias is represented as the ratio of the reported U5MR to the true U5MR.
+#'  \item IGME2019. Yearly Estimates of national under-5 child mortality in Malawi from the 2019 UN-IGME estimates.
+#' }  
+#' @references Neff Walker, Kenneth Hill, and Fengmin Zhao (2012) \emph{Child mortality estimation: methods used to adjust for bias due to aids in estimating trends in under-five mortality.}, \cr \emph{PLoS Medicine, 9(8):e1001298}.
+#' 
+#' @docType data
+#'
+#' @usage data(MalawiData)
+#' 
+"MalawiData"
--- R/MalawiMap.R
+++ R/MalawiMap.R
@@ -3,6 +3,6 @@
 #' SpatialPolygonsDataFrame objects that reflect the Admin 2 regions in Malawi, including the Likoma island. The Admin 2 region names are in the ADM2_EN field.
 #' 
 #' @docType data
-#' @usage data(malawiMap)
+#' @usage data(MalawiMap)
 
-"malawiMap"
+"MalawiMap"
