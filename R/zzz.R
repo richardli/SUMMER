4f9f0debf331a7115a4e3b0d413a1c8d0e72d902
Richard Li <lizehang@gmail.com>
2020-06-28 20:20:54 -0400

    on load update

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 .onAttach <- function( lib, pkg ) {
    packageStartupMessage(
       paste0("SUMMER version ", utils::packageDescription("SUMMER")$Version, "\n", 
-      		 "  See latest changes with 'news(package = 'SUMMER')'", "\n", 
-      		 "  See short vignette with ''"),
+      		 "  See latest changes with 'news(package = 'SUMMER')'"),
       domain = NULL,  appendLF = TRUE )
 }
