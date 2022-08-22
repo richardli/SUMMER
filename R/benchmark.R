a4bae6ca4a9fe644a9f9b6602d6306f49f570b8f
Richard Li <lizehang@gmail.com>
2022-08-22 11:29:38 -0700

    minor updates in benchmarking function

--- R/benchmark.R
+++ R/benchmark.R
@@ -11,6 +11,8 @@
 #' @return Benchmarked object in S3 class SUMMERproj or SUMMERprojlist in the same format as the input object \code{fitted}.
 #' @author Taylor Okonek, Zehang Richard Li 
 #' @importFrom stats runif
+#' @importFrom stats dnorm
+#' @importFrom stats rbinom
 #' @examples
 #' \dontrun{
 #' data(DemoData)
@@ -43,7 +45,7 @@
 #' national <- data.frame(years = periods, 
 #'					   est = seq(0.25, 0.15, length = 7), 
 #'					   sd = runif(7, 0.01, 0.03))
-# the row order does not matter as long as the years variable can be matched
+#' # the row order does not matter as long as the years variable can be matched
 #' national <- national[sample(1:7),]
 #' 
 #' # benchmarking
@@ -118,11 +120,118 @@
 #' # It can be seen the central region are pulled to the national benchmark
 #' subset(est.bb.bench$overall, region == "central")
 #' subset(est.bb$overall, region == "central") 
+#' 
+#' ## Example with the MH method
+#' periods <- c("85-89", "90-94", "95-99", "00-04", "05-09", "10-14", "15-19")
+#' # Benchmarking can be applied when customized priors are 
+#' #  specified for fixed effects when fitting the model
+#' fit.bb.new  <- smoothCluster(data = counts.all, Amat = DemoMap$Amat, 
+#'					family = "betabinomial",
+#'					year_label = periods, 
+#'					survey.effect = TRUE, 
+#'					control.fixed = list(
+#'						mean=list(`age.intercept0::1`=-4, 
+#'							       `age.intercept1-11::1`=-5,
+#'							       `age.intercept12-23::1`=-8,
+#'							       `age.intercept24-35::1`=-9,
+#'							       `age.intercept36-47::1`=-10,
+#'							       `age.intercept48-59::1`=-11), 
+#'						prec=list(`age.intercept0::1`=10, 
+#'							       `age.intercept1-11::1`=10,
+#'							       `age.intercept12-23::1`=10,
+#'							       `age.intercept24-35::1`=10,
+#'							       `age.intercept36-47::1`=10,
+#'							       `age.intercept48-59::1`=10)))
+#' est.bb.new <- getSmoothed(fit.bb.new, nsim = 1000, CI = 0.95, save.draws=TRUE)
+#' est.bb.bench.MH <- Benchmark(est.bb.new, national, 
+#' 											weight.region = weight.region, 
+#' 											estVar = "est", sdVar = "sd", timeVar = "years",
+#' 											method = "MH")
 #'  }
 #' @export
 #' 
 
 Benchmark <- function(fitted, national, estVar, sdVar, timeVar = NULL, weight.region = NULL, method = c("MH","Rejection")[2]) {
+
+	############################################################################
+	#  Helper functions
+	############################################################################
+
+	# This function grabs the fixed effect draws, 
+	#    this only applies to the fixed effects specified in control.fixed
+	# input: from getSmoothed()
+	# output: 
+	#   1. draws: K by Nsim matrix of draws of K named intercepts
+	#   2. priors: K by 2 matrix of prior mean and precision of the K named intercepts 
+	get_intercepts <- function(fitted){
+	  control <- fitted$control.fixed
+	  K <- length(control$mean)
+	  priors <- matrix(NA, K, 2)
+	  colnames(priors) <- c("mean", "prec")
+	  names <- names(control$mean)
+	  rownames(priors) <- names
+	  for(i in 1:K){
+	    priors[i, 1] <- control$mean[[i]]
+	    priors[i, 2] <- control$prec[[names[i]]]
+	  }	
+	  Nsim <-length(fitted$draws)
+	  draws <- matrix(NA, K, Nsim)
+	  rownames(draws) <- names
+	  for(i in 1:Nsim){
+	    draws[, i] <- fitted$draws[[i]]$latent[names, ]
+	  }
+	  colnames(draws) <- paste0("sample:", 1:Nsim)
+	  return(list(draws = draws, priors = priors))
+	  
+	}
+
+	## A_full: the function that calculates the acceptance probability given:
+	# old_thetas: current fitted values, on probability scale (not logit scale)
+	# new_thetas: proposed fitted values, on probability scale (not logit scale)
+	# old_betas: current intercepts
+	# new_betas: proposed intercepts
+	# z: national benchmarks, in order arrange(time)
+	# intercept_means: the means for the intercept terms that were input to inla()
+	# weights: population size weights, in order arrange(region, time), where they sum to 1 at each time point
+	# var_z: variances for the national benchmarks
+	# var_plus: the variances for the intercept terms that were input to inla() (VARIANCES, not precisions or sds)
+	# nregion: number of regions
+	# ntime: number of time points
+	# assumes thetas are in order arrange(region, time)
+	A_full <- function(old_thetas, 
+	                   new_thetas, 
+	                   old_betas,
+	                   new_betas,
+	                   z, 
+	                   intercept_means,
+	                   var_z, 
+	                   var_plus,
+	                   nregion,
+	                   ntime) {
+	  
+	  # initialize numerator and denominator
+	  num <- 1
+	  denom <- 1
+	  
+	  # add likelihoods for z
+	  for (i in 1:ntime) {
+	    num <- num * dnorm(z[i], new_thetas[i], sqrt(var_z[i]))
+	    denom <- denom * dnorm(z[i], old_thetas[i], sqrt(var_z[i]))
+	  }
+	  
+	  # add likelihoods for intercepts
+	  for (i in 1:length(old_betas)) {
+	    num <- num * dnorm(old_betas[i], intercept_means[i], sqrt(var_plus[i]))
+	    denom <- denom * dnorm(new_betas[i], intercept_means[i], sqrt(var_plus[i]))
+	  }
+	  # print(num/denom)
+	  return(min(1, num/denom))
+	}
+
+	############################################################################
+	#  Check input format
+	############################################################################
+
 	# Check input types
 	if(!is(fitted, "SUMMERprojlist")){
 		if(is(fitted, "SUMMERproj")){
@@ -198,6 +307,10 @@ Benchmark <- function(fitted, national, estVar, sdVar, timeVar = NULL, weight.re
 	}
 
 
+	############################################################################
+	#  Arrange input data
+	############################################################################
+
 	# Match benchmarked values
 	t_sub <- 1:ntime
 	nat <- data.frame(est = national[, estVar], 
@@ -233,8 +346,11 @@ Benchmark <- function(fitted, national, estVar, sdVar, timeVar = NULL, weight.re
 	# if benchmarking only happens in subset of years
 	q_mat <- q_mat[t_sub, ]
 
-  # if rejection algorithm...
-	if (method == "Rejection") {
+
+	############################################################################
+	#  Rejection algorithm
+	############################################################################
+  if (method == "Rejection") {
 	  # generate Us
 	  U <- runif(n0, 0, 1)
 	  
@@ -267,7 +383,9 @@ Benchmark <- function(fitted, national, estVar, sdVar, timeVar = NULL, weight.re
 	          round(length(acc) / n0, 3),
 	          "\n")
 	  
-	# if Metropolis-Hastings...
+	############################################################################
+	#  Metropolis-Hastings algorithm
+	############################################################################
 	} else {
 	  # get intercept draws and priors for fixed effects
 	  ints <- get_intercepts(fitted)
@@ -298,7 +416,7 @@ Benchmark <- function(fitted, national, estVar, sdVar, timeVar = NULL, weight.re
 	                          nregion = nregion,
 	                          ntime = ntime)
 	    
-	    accept_yn <- rbernoulli(n = 1, p = accept_prob)
+	    accept_yn <- rbinom(n = 1, size = 1, p = accept_prob)
 	    
 	    if (accept_yn) {
 	      accepted_ids <- c(accepted_ids, i)
